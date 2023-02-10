2f8cd470cb43ae18489122a953aea5d8514f253a
Hugge25 <111962627+KungeniDjunglen@users.noreply.github.com>
2023-02-10 11:03:30 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static List<int> MakeList(int tal)
+    {
+        List<int> AllaTal = new List<int>();
+        for (int i = 0; i < tal; i++)
+        {
+            AllaTal.Add(i);
+        }
+        return AllaTal;
+
+    }
+    static int PrimtalsUndersökarn(int tal)
+    {
+        List<int> AllaTal = MakeList(tal);
+        for (int i = 2; i < AllaTal.Count(); i++)
+        {
+            if (tal % i == 0)
+            {
+                AllaTal.Remove(i);
+            }
+        }
+        return AllaTal.Count;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Skriv ett tal så undersöker vi om det är ett primtal");
+        int tal = int.Parse(Console.ReadLine());
+        Console.WriteLine("Det finns: " + PrimtalsUndersökarn(tal));
+
+    }
+}
